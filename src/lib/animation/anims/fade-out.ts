1686ad15de28531a4018248b0720dbfeec45038d
jxmked <jovandeguia@gmail.com>
2023-02-23 11:52:25 +0800

    Flicker fixed

--- src/lib/animation/anims/fade-out.ts
+++ src/lib/animation/anims/fade-out.ts
@@ -1,15 +1,17 @@
 import AbsFading from '../abstracts/fading';
+import { flipRange } from '../../../utils';
 
 export class FadeOut extends AbsFading {
   public get value(): number {
-    if (this.isComplete && !this.isRunning) return 0;
+    if (this.isComplete || !this.isRunning) return 0;
 
-    const value = (this.time - this.startTime) / this.options.duration;
+    const value = this.timeDiff / this.options.duration;
 
     if (value >= 1) {
       this.stop();
+      return 0;
     }
 
-    return this.inUseTransition(1 - value);
+    return this.inUseTransition(flipRange(0, 1, value));
   }
 }
