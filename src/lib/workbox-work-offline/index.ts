05629e36e125c146d0573a0ba18efbe07e1ea780
jxmked <jovandeguia@gmail.com>
2023-11-29 15:22:51 +0800

    relative vs absolute pathgh-pages -d dist !

--- src/lib/workbox-work-offline/index.ts
+++ src/lib/workbox-work-offline/index.ts
@@ -5,7 +5,7 @@ export default () => {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker
-        .register('/service-worker.js')
+        .register('./service-worker.js')
         .then((registration) => {
           console.log('SW registered');
         })
